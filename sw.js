ffca97068385c1cee063c72c1fa11993fb71c326
qubise-bb8 <contact.mohammadhussain@gmail.com>
2026-09-25 10:50:43 +0530

    TAAZ V9.4 refresh MC summary cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='taaz-v9-3-voice-controls';
+const CACHE='taaz-v9-4-mc-whatsapp-summary';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
